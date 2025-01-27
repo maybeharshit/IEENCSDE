61179acefe2ee983fa3235cfb51768625ce58862
Harshit <168282584+maybeharshit@users.noreply.github.com>
2025-01-28 00:04:20 +0530

    Update Single_Number.c

--- Single_Number.c
+++ Single_Number.c
@@ -1,1 +1,9 @@
+//136. Single Number
 
+int singleNumber(int* nums, int numsSize) {
+    int temp = 0;
+    for(int i = 0;i<numsSize;i++){
+        temp = nums[i] ^ temp;
+    }
+    return temp;
+}
